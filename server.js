15e0fa05c7e049e82ffb445250341d9f87836af7
Dhananjai <dhananjaiyadav2006@gmail.com>
2025-04-15 09:13:21 +0530

    done with backend ca2

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+
+const port = 3000
+
+
+app.use(express.json())
+
+
+app.listen(port,()=>{
+    console.log(`server is running on the port ${port}`)
+})
+
+
+const users = [
+    {name:"dhananjai",age:25,email:"dhananajai@gmail.com"},
+    {name:"manoj",age:25,email:"dhananajai@gmail.com"}
+]
+
+
+
+
+app.get("user-info",(req,res)=>{
+    const name = req.query.name
+
+    if(!name){
+        res.status(400).json({
+            message:"enter a username"
+        })
+    }
+    const info = users(name)
+
+    const response = {
+        name:name,
+        age: info.age,
+        email: info.email
+    }
+
+    res.send(response)
+})
+
+
